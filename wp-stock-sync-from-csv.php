5fa4ae686d4530a858df5a5b46030402b9cf187a
Sourov Biswas <sourovcodes@gmail.com>
2025-12-23 16:02:59 +0600

    Bump version to 1.0.3 and update version constant

--- wp-stock-sync-from-csv.php
+++ wp-stock-sync-from-csv.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Stock Sync From CSV
  * Plugin URI: https://github.com/JonakyDS/wp-stock-sync-from-csv
  * Description: Automatically syncs WooCommerce product stock levels from a remote CSV file.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Jonaky Adhikary
  * Author URI: https://jonakyds.com
  * License: GPL v2 or later
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Plugin constants
-define( 'WSSFC_VERSION', '1.0.2' );
+define( 'WSSFC_VERSION', '1.0.3' );
 define( 'WSSFC_PLUGIN_FILE', __FILE__ );
 define( 'WSSFC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WSSFC_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
